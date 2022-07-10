6f866c4970e1ee2e531ac21887467fa2d7eaefd5
SShMeL-007 <108610664+SShMeL-007@users.noreply.github.com>
2022-07-10 13:14:31 +0300

    Update 01_task_except.py
    Новый апдейт

--- Module7/practice/exceptions/01_task_except.py
+++ Module7/practice/exceptions/01_task_except.py
@@ -4,3 +4,21 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+def S(a, b):
+    return a * b
+
+is_repeat_needed = True
+while is_repeat_needed:
+    nxm = input("Введите размеры прямоугольника, следующим видом: 12x5 - ")
+    nm = nxm.split("x")
+    if len(nm) == 1:
+        print("Не найден разделитель")
+    elif len(nm) > 2:
+        print("Чисел слишком много")
+    else:
+        try:
+            print(int(S(int(nm[0]), int(nm[1])) / S(int(nm[1]), int(nm[1]))))
+        except BaseException as ex:
+            print(ex)
+        else:
+            is_repeat_needed = False
